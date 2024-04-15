1300fe27ee9517fa9b34f28dd67da0a557c87e50
DaBultz <27208227+DaBultz@users.noreply.github.com>
2024-04-15 16:16:58 +0200

    play around with hl-chunk vs indent-blankline (unsure)

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,12 +1,47 @@
+local useHLChunk = false
+
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
+    enabled = not useHLChunk,
     config = function()
       require('ibl').setup {
+        scope = {
+          enabled = true,
+          show_start = true,
+          show_end = true,
+          char = '▏',
+        },
         -- See: :help ibl.config.indent.char
         indent = {
-          char = '▏',
+          char = '│',
+        },
+      }
+    end,
+  },
+  {
+    'shellRaining/hlchunk.nvim',
+    event = { 'UIEnter' },
+    enabled = useHLChunk,
+    config = function()
+      ---@diagnostic disable: missing-fields
+      require('hlchunk').setup {
+        ---@diagnostic disable-next-line: missing-fields
+        indent = {
+          enable = true,
+        },
+        blank = {
+          enable = true,
+        },
+        line_num = {
+          enable = false,
+        },
+        chunk = {
+          enable = true,
+        },
+        context = {
+          enable = false,
         },
       }
     end,
