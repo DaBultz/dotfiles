5a26c7a7479ab3eba715bf27a01417bf78079397
DaBultz <27208227+DaBultz@users.noreply.github.com>
2024-04-19 00:55:48 +0200

    setup a hidden files list for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,9 +1,32 @@
+local hidden_files = {
+  '..',
+  '.git',
+  'node_modules',
+  'zig-cache',
+  'zig-out',
+}
+
+local function has_value(tab, val)
+  for index, value in ipairs(tab) do
+    if value == val then
+      return true
+    end
+  end
+
+  return false
+end
+
 return {
   'stevearc/oil.nvim',
   config = function()
     require('oil').setup {
       keymaps = {
         ['..'] = 'actions.parent',
+      },
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          return has_value(hidden_files, name)
+        end,
       },
     }
   end,
