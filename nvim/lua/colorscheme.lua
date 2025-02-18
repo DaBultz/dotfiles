ee8c1334d0adaf20109c78fa5c8d86ef5c990777
DaBultz <27208227+DaBultz@users.noreply.github.com>
2025-02-18 07:49:42 +0100

    update

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,2 +1,3 @@
 -- vim.o.background = "dark"
-vim.cmd.colorscheme("monoglow")
+-- vim.cmd.colorscheme("monoglow")
+vim.cmd.colorscheme("rose-pine")
--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -3,4 +3,6 @@ Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 Set-Alias -Name 'ls' -Value eza.exe
 Set-Alias -Name 'zvmup' -Value 'zvm i --zls --full master'
 
+$env:PATH += ";~\AppData\Local\nvim-data\mason\bin"
+
 oh-my-posh init pwsh --config ~/.oh-my-posh/config.json | Invoke-Expression
