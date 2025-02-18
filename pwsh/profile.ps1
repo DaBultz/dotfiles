c0e491a28f91a34f8b6af2d930419c1137c6bbb7
DaBultz <27208227+DaBultz@users.noreply.github.com>
2025-02-18 07:30:02 +0100

    update

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,1 +1,5 @@
 Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
+Set-Alias -Name 'ls' -Value eza.exe
+
+oh-my-posh init pwsh --config ~/.oh-my-posh/config.json | Invoke-Expression
