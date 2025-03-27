7d5f7bf52689eff3496896e790bfa0fad72a268d
DaBultz <27208227+DaBultz@users.noreply.github.com>
2025-03-27 03:49:51 +0100

    update

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,12 +1,20 @@
 return {
 	{
 		"supermaven-inc/supermaven-nvim",
-		enabled = true,
+		enabled = false,
 		config = function()
 			require("supermaven-nvim").setup({
 				disable_inline_completion = true, -- disables inline completion for use with cmp
 				disable_keymaps = true, -- disables built in keymaps for more manual control
 			})
+		end,
+	},
+	{
+		"augmentcode/augment.vim",
+		config = function()
+			vim.g.augment_workspace_folders = {
+				"~/projects/zdb-api",
+			}
 		end,
 	},
 }
