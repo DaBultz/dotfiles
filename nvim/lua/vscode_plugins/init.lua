a0d9c76040a3f729dab096e7bba659d4b0a314ee
DaBultz <27208227+DaBultz@users.noreply.github.com>
2025-03-28 04:16:29 +0100

    update

---
+++ nvim/lua/vscode_plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		-- See: :h nvim-surround.usage
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"chrisgrieser/nvim-spider",
+		keys = {
+			{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+			{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+			{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+		},
+		config = function()
+			require("nvim-spider").setup({})
+		end,
+	},
+}
